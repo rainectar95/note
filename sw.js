2d42c7d006fa047838d45b85b0bf5940132504f6
unknown <rainectar95>
2026-02-02 01:00:43 +0300

    Первая версия приложения

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'kopek-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Установка: кэшируем файлы
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Запрос: отдаем из кэша, если интернета нет
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
